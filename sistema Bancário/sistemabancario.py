12ff5aec5f68cc3ad03832d7fc6c076e52785d0b
luiz-eirado <dev.eirado@gmail.com>
2024-08-01 16:50:31 -0300

    finalizando logica da opcao sacar.

--- sistema Bancário/sistemabancario.py
+++ sistema Bancário/sistemabancario.py
@@ -10,10 +10,11 @@ menu = """
 saldo = 0 
 limite = 500
 extrato = ""
+numero_de_saques = 0
 LIMITE_SAQUES = 3
 
 
-while true:
+while True:
 
     opcao = input(menu)
 
@@ -26,6 +27,36 @@ while true:
 
         else:
             print("Operação falhou! O valor informado é inválido.")    
+
+    elif opcao == "2":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_de_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_de_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+                
+
+
+        
 
 
 
