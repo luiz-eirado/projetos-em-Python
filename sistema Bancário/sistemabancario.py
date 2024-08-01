f0120a1fcc501a49eb37eaa4bdb0e856a3cf6f2f
luiz-eirado <dev.eirado@gmail.com>
2024-08-01 16:30:05 -0300

    Construindo a lógica do sistema.

--- sistema Bancário/sistemabancario.py
+++ sistema Bancário/sistemabancario.py
@@ -1,4 +1,4 @@
-"""menu = 
+menu = """
 
 [1] depositar
 [2] sacar 
@@ -11,5 +11,21 @@ saldo = 0
 limite = 500
 extrato = ""
 LIMITE_SAQUES = 3
+
+
+while true:
+
+    opcao = input(menu)
+
+    if opcao == "1":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")    
+
 
 
